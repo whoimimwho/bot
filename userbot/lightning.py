d1730d12b9075785a2e6772e06b2635fc9f5a492
whoimimwho <79775567+whoimimwho@users.noreply.github.com>
2021-08-26 14:06:58 +0530

    Update lightning.py

--- userbot/lightning.py
+++ userbot/lightning.py
@@ -5,17 +5,17 @@
 #chala jaa bhosdike 
 import os
 from userbot import ALIVE_NAME 
-MASTER = str(ALIVE_NAME) if ALIVE_NAME else "LIGHTNING"
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "USERBOT"
 NAME = os.environ.get("BOT_NICK_NAME")
 BOT =  = str(NAME) if NAME else "BLACK LIGHTNING"
 #op for keinshin
 
-Keinshin = "[KEINSHIN](https://t.me/keinshin)"
-OP = "[BLACK LIGHTNING](https://github.com/KeinShin/Black-Lightning)"
-OKAY = "[SUPPORT GROUP](https://t.me/LIGHTINGSUPPORT)"
+Keinshin = "[AKASH](https://t.me/BeingImimmortal)"
+OP = "[LOL](https://github.com)"
+OKAY = "[SUPPORT GROUP](https://t.me/BeingImimmortal)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
-ALIVE = "BLACK LIGHTNING BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
+ALIVE = "USER BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
 USERBOT = " HELLO MASTER MY NAME IS BLACK LIGHTNING BOT I AM A BEST USERBOT 💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
